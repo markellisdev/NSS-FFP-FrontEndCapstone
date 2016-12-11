84bc821fa68407c0b7218e1a718b543b5e7acb48
Mark Ellis <markellisdev@gmail.com>
2016-12-10 21:46:34 -0600

    firebase and factories setup to be tested

--- app/app.js
+++ app/app.js
@@ -1,1 +1,15 @@
 "use strict";
+
+var app = angular.module ("FFP-APP", [ng-route]);
+
+/*-- Authenticate User --*/
+let isAuth = (AuthFactory) => new Promise ( (resolve, reject) => {
+	AuthFactory.isAuthenticated()
+	.then ( (userExists) => {
+		if(userExists) {
+			resolve();
+		} else {
+			reject();
+		}
+	});
+});
