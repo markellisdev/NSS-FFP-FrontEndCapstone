e15e3fbdd263ad1d267db756f6c113ef08cb3f70
Mark Ellis <markellisdev@gmail.com>
2016-12-13 10:27:01 -0600

    login loop bug fix

--- app/controllers/NavCtrl.js
+++ app/controllers/NavCtrl.js
@@ -2,21 +2,20 @@
 
 app.controller("NavCtrl", function(AuthFactory, $scope, $window){
 	$scope.isLoggedIn = false;
-
 	firebase.auth().onAuthStateChanged( function(user){
 		if (user) {
 			$scope.isLoggedIn = true;
-			console.log("current user logged in", user, $scope.isLoggedin);
+			console.log("current user logged in", user, $scope.isLoggedIn);
 			$scope.$apply();
 		}else{
 			$scope.isLoggedIn = false;
-			console.log("current user logged in", $scope.isLoggedin);
-			$window.location.href = '#/login';
+			console.log("current user logged in", $scope.isLoggedIn);
+			// $window.location.href = '/login';
 		}
 	});
 
 	$scope.logout = () => {
-		AuthFactory.logoutUser()
+		AuthFactory.logoutUser();
 	};
 
 
