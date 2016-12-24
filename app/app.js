59e2025597fd34694e8e0b18177e158e67b464fe
Mark Ellis <markellisdev@gmail.com>
2016-12-23 21:53:06 -0600

    angular-ui-router working

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 "use strict";
 
-var app = angular.module("FFP-APP", ["ngRoute"]);
+var app = angular.module("FFP-APP", ["ui.router"]);
+
+// cut from above to use ui.router , "ngRoute"
 
 /*-- Authenticate User --*/
 let isAuth = (AuthFactory) => new Promise ( (resolve, reject) => {
@@ -14,30 +16,95 @@ let isAuth = (AuthFactory) => new Promise ( (resolve, reject) => {
 	});
 });
 
-app.config(function($routeProvider) {
-	$routeProvider
-	.when('/login', {
-		templateUrl: 'partials/login.html',
-		controller: 'LoginCtrl'
-	})
-	.when('/test', {
+app.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+    	.state('root',{
+    		url: '',
+    		views: {
+    			'header': {
+    				templateUrl: 'partials/navbar.html',
+    				controller: 'NavCtrl'
+    			}
+    		}
+    	})
+        .state('root.login', {
+			url: '/login',
+			views: {
+				'container@': {
+					templateUrl: 'partials/login.html',
+					controller: 'LoginCtrl'
+				}
+			}
+
+		})
+		.state('root.home', {
+		  url: '/',
+		  views: {
+		    'container@': {
+		      templateUrl: 'partials/home.html'
+		    }
+		  }
+		})
+		.state('root.team', {
+		  url: '/team',
+		  views: {
+		    'container@': {
+		      templateUrl: 'partials/teamView.html',
+		      controller: 'PlayerListCtrl'
+		    }
+		  }
+		})
+        .state('/test', {
 		templateUrl: 'partials/test.html',
 		controller: 'AddClubCtrl'
-	})
-	.when('/team', {
-		templateUrl: 'partials/teamView.html',
-		controller: 'PlayerListCtrl'
-	})
-	.when('/home', {
-		templateUrl: 'partials/home.html'
-	})
-	.otherwise('/home')
-;
-// This bit of code below makes it so that routing works without !(bang)
-}).config(function($locationProvider){
-	$locationProvider.html5Mode(true);
+		})
+		// .state('root.team', {
+		// templateUrl: 'partials/teamView.html',
+		// controller: 'PlayerListCtrl'
+		// })
+		// .state('/home', {
+		// templateUrl: 'partials/home.html'
+		// })
+		.state('/userTeam', {
+		templateUrl: 'partials/userTeamView.html',
+		controller: 'AddTeamCtrl'
+		});
 });
 
+// app.config(function($routeProvider) {
+// 	$routeProvider
+// 	.when('/login', {
+// 		templateUrl: 'partials/login.html',
+// 		controller: 'LoginCtrl'
+// 	})
+// 	.when('/test', {
+// 		templateUrl: 'partials/test.html',
+// 		controller: 'AddClubCtrl'
+// 	})
+// 	.when('/team', {
+// 		templateUrl: 'partials/teamView.html',
+// 		controller: 'PlayerListCtrl'
+// 	})
+// 	.when('/home', {
+// 		templateUrl: 'partials/home.html'
+// 	})
+// 	.when('/userTeam', {
+// 		templateUrl: 'partials/userTeamView.html',
+// 		controller: 'AddTeamCtrl'
+// 	})
+// 	.otherwise('/home')
+// ;
+// // This bit of code below makes it so that routing works without !(bang)
+// }).config(function($locationProvider){
+// 	$locationProvider.html5Mode(true);
+// });
+app.run(['$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams) {
+  $rootScope.$state = $state;
+  $rootScope.$stateParams = $stateParams;
+  $state.transitionTo('root.home');
+}]);
 
 app.run( ($location, FBCreds) => {
 	let creds = FBCreds;
