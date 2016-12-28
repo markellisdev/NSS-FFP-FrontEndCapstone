139b36dd8f0b91a1b6f770ee9fee3e9b61ec55d4
Mark Ellis <markellisdev@gmail.com>
2016-12-28 11:16:06 -0600

    This loop is ridiculous

--- app/controllers/PlayerListCtrl.js
+++ app/controllers/PlayerListCtrl.js
@@ -22,9 +22,66 @@ app.controller('PlayerListCtrl', function($scope, TeamStorage, $location, AuthFa
 		$scope.$apply();
 	});
 
+// Function to return completed matches, i.e. only matches that reached FullTime
+	function isFullTime(match) {
+	  return match.MatchInfo.Period === 'FullTime';
+	}
+
+// Function to group matches in Match Weeks (referred to here as MatchDay i.e. week 1, 2, 3, etc)
+	function groupByMatchDay(total, current) {
+		// console.log("groupByMatchDay function inside", total[current.MatchInfo.MatchDay]);
+	  if (total[current.MatchInfo.MatchDay]) {
+	    total[current.MatchInfo.MatchDay].push(current.TeamData)
+	  } else {
+	    total[current.MatchInfo.MatchDay] = [current.TeamData]
+	  }
+	  return total;
+	}
+
+// Return matches where goals were scored
+	function hasGoals(match) {
+	  return match.Goal;
+	}
+
+// Function to get goals from each match
+	function getMatchGoals(match){
+	  return match
+	    .filter(hasGoals)
+	    .map(match => match.Goal)
+	    .reduce((total, current) => {
+	      return total.concat(current.map(c => c.PlayerRef));
+	    }, []);
+	}
+
+// Function to calculate players score
+	function calculatePlayerScore(total, player) {
+	    if (total.hasOwnProperty(player)) {
+	      total[player] += 5;
+	    } else {
+	      total[player] = 5;
+	    }
+
+	    return total;
+	  }
+
+// Function to flatten an array
+	function flatten(arr) {
+	  return arr.reduce((flat, toFlatten) => {
+	    return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+	  }, []);
+	}
+
+//This will get matches that reached fulltime and group by matchday
 	let p3 = MatchFactory.getMatches() //Need to write this function
-	.then( (matchArray) => {
-		$scope.matches = matchArray[0].MatchData;
+	.then( (matchData) => {
+		return matchData
+			.filter(isFullTime)
+			.reduce(groupByMatchDay, [])
+			.map(x => flatten(x.map(getMatchGoals)).reduce(calculatePlayerScore, {}))
+	})
+	.then(matchData => {
+		console.log("matchData after reducing by matchday", matchData);
+		$scope.matches = matchData;
 		$scope.$apply();
 	});
 
@@ -33,25 +90,13 @@ app.controller('PlayerListCtrl', function($scope, TeamStorage, $location, AuthFa
 	$scope.sortReverse  = false;  // set the default sort order
 	$scope.selectProps = ["Season Points", "5 Week Form", "3 Week Form"];
 
-
 	// Nothing after this happens until all promises are returned, players, clubs and matches
 	Promise.all([p1, p2, p3]).then(() => {
-		console.log("Did promise dot all work? ", $scope.matches);
+
 		let Gameweek = [];
+		let uID = "-uID";
 
-		for (let n=0; n < $scope.matches.length; n++) {
-		// 	// Evaluate only matches that reached FullTime (those completed)
-			if($scope.matches[n].MatchInfo.Period == "FullTime") {
-
-				let md = $scope.matches[n].MatchInfo.MatchDay;
-		// This  changes the gameweek number to a word, 3=three
-				let mdWord = NumFactory.toWords(md);
-				console.log("mdWord", md, mdWord);
-		// Create and array of Gameweeks to be populated by score data later
-				Gameweek[mdWord] = 0;
-			}
-		}
-
+		//This adds the proper team data to each player
 		for (let x=0; x < $scope.players.length; x++) {
 			let tempID = $scope.players[x].clubID;
 
@@ -68,112 +113,44 @@ app.controller('PlayerListCtrl', function($scope, TeamStorage, $location, AuthFa
 			}
 		}
 
+		//This iterates through matches to add points per week to players
 
-
-
-		// This is to loop through all MatchData in Firebase
-		for (let n=0; n < $scope.matches.length; n++) {
-			// console.log("matches.length working??", $scope.matches[n].MatchInfo.MatchDay);
-		let weekNum = $scope.matches[n].MatchInfo.MatchDay;
-		let weekWord = NumFactory.toWords(weekNum);
-			// If the MatchDay == user's selection
-			// -----------------------
-/*-- There's still a problem with the logic here and points totals --*/
-/*-- --------------------------------------------------------------------------------------- --*/
-			if ($scope.matches[n].MatchInfo.MatchDay == weekNum) {
-				let arr = $scope.matches[n].MatchInfo.MatchDay;
-				console.log("array of Matches for week ", arr);
-
-				let TeamData = $scope.matches[n].TeamData;
-
-				// For each match, count goals
-				for (let nn=0; nn < TeamData.length; nn++) {
-					//If there are goals
-					if (TeamData[nn].Goal) {
-						let GoalData = TeamData[nn].Goal;
-						// For each goal get PlayerRef
-						for (let nnn=0; nnn < GoalData.length; nnn++) {
-							let goalScorer = GoalData[nnn].PlayerRef;
-							// Loop through players
-							for(let p=0; p < $scope.players.length; p++) {
-								let uID = "-uID";
-								// When goalScorer == the players uID, add points accordingly
-								if (goalScorer == $scope.players[p][uID]) {
-									// Ternary to evaluate if there are already weekPoints. If there are, add to them
-									// $scope.players[p].weekPoints = $scope.players[p].weekPoints ? $scope.players[p].weekPoints : 0;
-									$scope.players[p].Gameweeks[weekWord] = $scope.players[p].Gameweeks[weekWord] ? $scope.players[p].Gameweeks[weekWord] : 0;
-									$scope.players[p].Gameweeks[weekWord] += 5;
-									// $scope.players[p].weekPoints += 5;
-									$scope.$apply();
-									console.log("The goal scorer is players", p, $scope.players[p] );
-									// debugger;
-								}
-							}
-							// $scope.players[]
-						}
+		for (let n=1; n <= $scope.matches.length; n++) {
+			// For each key, which should be a player id
+			for (let key in $scope.matches[n]) {
+				console.log("Which match? ", key);
+				// For each player, compare key to players uID
+				for(let nn=0; nn < $scope.players.length; nn++) {
+					if (key === $scope.players[nn][uID]) {
+						console.log("Which player was the first to equal key? ", $scope.players[nn][uID]);
+						debugger;
+						console.log(key, " this is the key", $scope.matches[n][key]);
+						$scope.players[nn].Gameweeks[n] = $scope.matches[n][key];
+						console.log("First player to score gets ", $scope.matches[n][key], " points!", $scope.players[nn]);
 					}
-
 				}
+			    // `prop` contains the name of each property, i.e. `'code'` or `'items'`
+			    // consequently, `data[prop]` refers to the value of each property, i.e.
+			    // either `42` or the array
 			}
 		}
 
-		// for (let n=0; n < $scope.matches.length; n++) {
-		// 	// Evaluate only matches that reached FullTime (those completed)
-		// 	if($scope.matches[n].MatchInfo.Period == "FullTime") {
-
-		// 		let md = $scope.matches[n].MatchInfo.MatchDay;
-		// This  changes the gameweek number to a word, 3=three
-		// 		let mdWord = NumFactory.toWords(md);
-		// 		console.log("mdWord", md, mdWord);
-		// Create and array of Gameweeks to be populated by score data later
-		// 		let Gameweek = [];
-		//		this.addGameweek = (mdWord) => {
-		//			if (mdWord) {
-		//				this.gweek = {
-		//					[mdWord] = 0
-		//				};
-		//			})
-		//		});
-		//
-		//
-		//
-
-		// 		for (let nn=0; nn < TeamData.length; nn++) {
-		// 			//If there are goals
-		// 			if (TeamData[nn].Goal) {
-		// 				let GoalData = TeamData[nn].Goal;
-		// 				// For each goal get PlayerRef
-		// 				for (let nnn=0; nnn < GoalData.length; nnn++) {
-		// 					let goalScorer = GoalData[nnn].PlayerRef;
-		// 					// Loop through players
-		// 					for(let p=0; p < $scope.players.length; p++) {
-		// 						let uID = "-uID";
-		// 						// When goalScorer == the players uID, add points accordingly
-		// 						if (goalScorer == $scope.players[p][uID]) {
-		// 							// Ternary to evaluate if there are already weekPoints. If there are, add to them
-		// 							$scope.players[p].weekPoints = $scope.players[p].weekPoints ? $scope.players[p].weekPoints : 0;
-		// 							$scope.players[p].weekPoints += 5;
-		// 							$scope.$apply();
-		// 							console.log("The goal scorer is players", p, $scope.players[p] );
-		// 							debugger;
-		// 						}
-		// 					}
-		// 					// $scope.players[]
-		// 				}
-		// 			}
-
-		// 		}
-
-		// 	}
-		// }
+		// $scope.keys(data).forEach(function(prop) {
+		//   // `prop` is the property name
+		//   // `data[prop]` is the property value
+		// });
 
 	});
 
-
-
-
-
-
-
-
 });
+
+// for (let nn=0; nn < $scope.players.length; nn++) {
+// 	console.log("$scope.matches", $scope.matches[n]);
+// 	break;
+// 	if ($scope.players[nn].uID == $scope.matches[n]) {
+// 		console.log("Which player is first?", $scope.players[nn]);
+// 		debugger;
+// 		$scope.players[nn].Gameweeks[n].append($scope.matches[n][$scope.players[nn]]);
+// 	}
+// console.log("Which player scored", $scope.players[nn]);
+// }
--- app/factories/MatchFactory.js
+++ app/factories/MatchFactory.js
@@ -6,7 +6,7 @@ app.factory("MatchFactory", ($http, FBCreds) => {
 
 		let Matches = [];
 		return new Promise(function(resolve, reject){
-			$http.get(`${FBCreds.databaseURL}/SoccerFeed.json`)
+			$http.get(`${FBCreds.databaseURL}/SoccerFeed/SoccerDocument/MatchData.json`)
 			.success((matchObj) => {
 				for (var match in matchObj) {
 					Matches.push(matchObj[match]);
