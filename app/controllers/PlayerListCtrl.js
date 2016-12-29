47d62104b5bed3076e28247ff521263454a57acd
Mark Ellis <markellisdev@gmail.com>
2016-12-29 15:11:06 -0600

    display working, need to convert num to string

--- app/controllers/PlayerListCtrl.js
+++ app/controllers/PlayerListCtrl.js
@@ -3,13 +3,13 @@
 app.controller('PlayerListCtrl', function($scope, TeamStorage, $location, AuthFactory, ClubFactory, MatchFactory, NumFactory, TeamFactory){
     console.log("Is PlayerListCtrl running here? ");
     let currentUser = AuthFactory.getUser();
-    let players;
+    let donkeys;
     $scope.players = [];
     $scope.clubs = [];
     $scope.matches = [];
     let p1 = TeamStorage.getPlayerList()
     .then( (playerArray) => {
-        players = playerArray;
+        donkeys = playerArray;
     });
     let p2 = ClubFactory.getClubs()
     .then( (clubsArray) => {
@@ -79,19 +79,19 @@ app.controller('PlayerListCtrl', function($scope, TeamStorage, $location, AuthFa
     Promise.all([p1, p2, p3]).then(() => {
         let uID = "-uID";
         //This adds the proper team data to each player
-        for (let x=0; x < players.length; x++) {
-            let tempID = players[x].clubID;
+        for (let x=0; x < donkeys.length; x++) {
+            let tempID = donkeys[x].clubID;
             for (let xx=0; xx < $scope.clubs.length; xx++) {
                 if ($scope.clubs[xx].uID === tempID) {
-                    players[x].code = $scope.clubs[xx].code;
-                    players[x].key = $scope.clubs[xx].key;
-                    players[x].name = $scope.clubs[xx].name;
-                    players[x].primaryColour = $scope.clubs[xx].primaryColour;
-                    players[x].secondaryColour = $scope.clubs[xx].secondaryColour;
+                    donkeys[x].code = $scope.clubs[xx].code;
+                    donkeys[x].key = $scope.clubs[xx].key;
+                    donkeys[x].name = $scope.clubs[xx].name;
+                    donkeys[x].primaryColour = $scope.clubs[xx].primaryColour;
+                    donkeys[x].secondaryColour = $scope.clubs[xx].secondaryColour;
                 }
             }
         }
-        // console.log("players", players);
+        // console.log("donkeys", donkeys);
         console.log("matchData", $scope.matches);
         //This iterates through matches to add points per week to players
         for (let n=15; n <= $scope.matches.length; n++) {
@@ -99,42 +99,25 @@ app.controller('PlayerListCtrl', function($scope, TeamStorage, $location, AuthFa
             for (let playerId in $scope.matches[n]) {
                 console.log("Which player / match week? ", playerId, n);
                 // For each player, compare key to players uID
-                for(var nn=0; nn < players.length; nn++) {
-                    if (playerId === players[nn][uID]) {
-                        players[nn].playerScores = players[nn].playerScores ? players[nn].playerScores : [];
-                        // console.log("player matched the score", nn, playerId, $scope.matches[n][playerId]);
-                        // console.log("Which player? ", players[nn].Name);
-                        players[nn].playerScores.push($scope.matches[n][playerId]);
-                        // console.log("Is this the player?", players[nn]);
+                for(var nn=0; nn < donkeys.length; nn++) {
+                    if (playerId === donkeys[nn][uID]) {
+                        donkeys[nn].playerScores = donkeys[nn].playerScores ? donkeys[nn].playerScores : [];
+                        console.log("player matched the score", nn, playerId, $scope.matches[n][playerId]);
+                        console.log("Which player? ", donkeys[nn].Name);
+                        donkeys[nn].playerScores.push($scope.matches[n][playerId]);
+                        console.log("Is this the player?", donkeys[nn]);
                     }
                 }
-                // `prop` contains the name of each property, i.e. `'code'` or `'items'`
-                // consequently, `data[prop]` refers to the value of each property, i.e.
-                // either `42` or the array
             }
         }
-        console.log("loops done, player array", players);
-        for (let ap=0; ap < players.length; ap++) {
-        	if (players[ap].playerScores) {
-	        	$scope.players.push(players[ap]);
-	        console.log("These players scored", $scope.players, "and these are the matches", $scope.matches);}
+        for (let z=0; z < donkeys.length; z++) {
+            if (donkeys[z].playerScores) {
+                $scope.players.push(donkeys[z]);
+                $scope.$apply();
+            }
         }
-
-
-
-        // $scope.keys(data).forEach(function(prop) {
-        //   // `prop` is the property name
-        //   // `data[prop]` is the property value
-        // });
+        console.log("loops done, player array", $scope.players);
+        // $scope.players = donkeys;
+        // $scope.$apply();
     });
 });
-// for (let nn=0; nn < players.length; nn++) {
-//  console.log("$scope.matches", $scope.matches[n]);
-//  break;
-//  if (players[nn].uID == $scope.matches[n]) {
-//      console.log("Which player is first?", players[nn]);
-//      debugger;
-//      players[nn].Gameweeks[n].append($scope.matches[n][players[nn]]);
-//  }
-// console.log("Which player scored", players[nn]);
-// }
